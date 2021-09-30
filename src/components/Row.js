cad64bc46a0e272594da493bdf208d83ff3aa9a2
Andrew Watson <Andrew.justinw@gmail.com>
2021-09-30 16:26:00 -0400

    Created a map function that will hold each indiviual returned movie's information

--- src/components/Row.js
+++ src/components/Row.js
@@ -3,27 +3,35 @@ import axios from "../data/axios"  // This is using axios.create we've created i
 
 const Row = ({ title, fetchURL }) => {
 
-    const [data, setData] = useState([]);
+    const [movies, setMovies] = useState([]);
 
     // Pulling in data using fetchURL inside of useEffect
     useEffect(() => {
         const fetchData = async () => {
 
             const req = await axios.get(fetchURL);
-            console.log(title, req)
+            setMovies(req.data.results)
         }
 
         fetchData()
 
-    }, [])
+    }, [fetchURL])
 
+    console.log(movies)
 
     return (
-        <div>
+        <div className="row">
 
             <h2>{title}</h2>
 
-            {/* container -> posters */}
+            <div className="row-container">
+                {movies.map((movie) => (
+
+                    <img src={movie.poster_path} alt={movie.name} />
+
+                ))}
+
+            </div>
 
         </div>
     )
