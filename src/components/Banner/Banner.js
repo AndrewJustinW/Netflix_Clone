7a1ad2484dd4c8f1c2da90c02720c6c765dd3014
Andrew Watson <Andrew.justinw@gmail.com>
2021-10-01 12:06:48 -0400

    Added styling to change banner background image dynamically depending on random movie

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -14,7 +14,6 @@ const Banner = () => {
 
             // This returns 1 random movie from the array of objects and sets it as the state of movie
             setMovie(req.data.results[Math.floor(Math.random() * req.data.results.length) - 1])
-
             return req
         }
 
@@ -22,8 +21,20 @@ const Banner = () => {
     }, [])
 
     return (
-        <header className="banner">
+        <header className="banner"
+            style={{
+                backgroundSize: "cover",
+                //Set backround image dynamically depending on movie
+                backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+            }}
+        >
 
+            <div className="banner-contents"></div>
+
+            <h1>
+                {/* Set Movie title but account for possible naming inconsistincies in API */}
+                {movie?.title || movie?.name || movie?.original_name}
+            </h1>
             {/* title */}
             {/* div with 2 buttons */}
             {/* description of featured movie */}
