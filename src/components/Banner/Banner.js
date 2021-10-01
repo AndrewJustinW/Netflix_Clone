fec84dc0127b29465ed64eb77997479654c98984
Andrew Watson <Andrew.justinw@gmail.com>
2021-09-30 23:44:37 -0400

    Created a request to netflix originals endpoint, grabbed one random movie from the array and set it to the movie state

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from '../../data/axios'
+import requests from '../../data/requests'
 
 const Banner = () => {
+
+    const [movie, setMovie] = useState([])
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+            const req = await axios.get(requests.fetchNetflixOriginals)
+            // console.log(req.data.results[Math.floor(Math.random() * req.data.results.length) - 1])
+
+            // This returns 1 random movie from the array of objects and sets it as the state of movie
+            setMovie(req.data.results[Math.floor(Math.random() * req.data.results.length) - 1])
+
+            return req
+        }
+
+        fetchData()
+    }, [])
+
     return (
         <header className="banner">
 
